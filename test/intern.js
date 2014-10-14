ee95a6924cc4e44d67f8726bc94f67a5d146b06f
Marcus Zanona <zanona@me.com>
2014-10-14 13:41:51 +0100

    Change Browserstack now being used for testing

--- test/intern.js
+++ test/intern.js
@@ -4,16 +4,17 @@ define({
     proxyUrl: 'http://localhost:9000/',
     capabilities: { 'selenium-version': '2.41.0' },
     environments: [
-        { browserName: 'internet explorer', version: '11', platform: 'Windows 8.1' },
-        { browserName: 'internet explorer', version: '10', platform: 'Windows 8' },
-        { browserName: 'internet explorer', version: '9',  platform: 'Windows 7' },
-        { browserName: 'firefox',           version: '28', platform: [ 'OS X 10.9', 'Windows 7', 'Linux' ] },
-        { browserName: 'chrome',            version: '34', platform: [ 'OS X 10.9', 'Windows 7', 'Linux' ] },
-        { browserName: 'safari',            version: '6',  platform: 'OS X 10.8' },
-        { browserName: 'safari',            version: '7',  platform: 'OS X 10.9' }
+        { browserName: 'internet explorer', version: '8',  platform: 'WINDOWS' }
+        { browserName: 'internet explorer', version: '11', platform: 'WINDOWS' },
+        { browserName: 'internet explorer', version: '10', platform: 'WINDOWS' },
+        { browserName: 'internet explorer', version: '9',  platform: 'WINDOWS' },
+        { browserName: 'firefox',           version: '28', platform: [ 'MAC', 'WINDOWS' ] }
+        { browserName: 'chrome',            version: '34', platform: [ 'MAC', 'WINDOWS' ] },
+        { browserName: 'safari',            version: '6',  platform: 'MAC' },
+        { browserName: 'safari',            version: '7',  platform: 'MAC' }
     ],
-    maxConcurrency: 3,
-    tunnel: 'SauceLabsTunnel',
+    maxConcurrency: 2,
+    tunnel: 'BrowserStackTunnel',
     loader: { packages: [ { name: './objPath', location: '.' } ] },
     suites: [ 'test/objPath' ],
     excludeInstrumentation: /^(?:test|node_modules)\//
