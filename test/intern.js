2ff7a0bb4ae22d152bdd98997254c1814bdf9800
Marcus Zanona <zanona@me.com>
2014-10-14 13:43:56 +0100

    Fix syntax error on intern config file

--- test/intern.js
+++ test/intern.js
@@ -4,11 +4,11 @@ define({
     proxyUrl: 'http://localhost:9000/',
     capabilities: { 'selenium-version': '2.41.0' },
     environments: [
-        { browserName: 'internet explorer', version: '8',  platform: 'WINDOWS' }
+        { browserName: 'internet explorer', version: '8',  platform: 'WINDOWS' },
         { browserName: 'internet explorer', version: '11', platform: 'WINDOWS' },
         { browserName: 'internet explorer', version: '10', platform: 'WINDOWS' },
         { browserName: 'internet explorer', version: '9',  platform: 'WINDOWS' },
-        { browserName: 'firefox',           version: '28', platform: [ 'MAC', 'WINDOWS' ] }
+        { browserName: 'firefox',           version: '28', platform: [ 'MAC', 'WINDOWS' ] },
         { browserName: 'chrome',            version: '34', platform: [ 'MAC', 'WINDOWS' ] },
         { browserName: 'safari',            version: '6',  platform: 'MAC' },
         { browserName: 'safari',            version: '7',  platform: 'MAC' }
